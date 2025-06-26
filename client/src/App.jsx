fe0e75edea0427d5b25402b9184214ce0a84f0d7
Rohit <rohitpatilwork7797@gmail.com>
2025-06-26 21:03:24 +0530

    Add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import './App.css'
-import Home from './components/Home/Home'
+import { Routes, Route } from 'react-router-dom'
+import Home from "./components/Home/Home";
+import RestaurantItems from './components/RestaurantItems';
 
-export default function App(){
-    return(
-        <Home />
-    )
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/item" element={<RestaurantItems />} />
+        </Routes>
+    );
 }
