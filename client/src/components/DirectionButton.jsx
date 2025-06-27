84538cd227951c42fc059b1723a61277fbe0af6f
Rohit <rohitpatilwork7797@gmail.com>
2025-06-27 19:40:28 +0530

    Add direction

--- client/src/components/DirectionButton.jsx
+++ client/src/components/DirectionButton.jsx
@@ -3,7 +3,7 @@ import DirectionsIcon from '@mui/icons-material/Directions';
 
 export default function DirectionButton() {
     return(
-        <button className="mt-4 md:mt-0 flex items-center gap-2 bg-amber-600 hover:bg-amber-700 text-white px-4 py-2 rounded-lg font-medium transition-colors">
+        <button className="mt-4 md:mt-0 flex items-center gap-2 bg-yellow-500 hover:bg-yellow-700 text-white px-4 py-2 rounded-lg font-medium transition-colors">
             <DirectionsIcon />
             <span>Get Directions</span>
         </button>
